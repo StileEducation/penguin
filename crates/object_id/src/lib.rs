cfb0c27673cece7e2c6cd1548bfa51f35f69149a
Nick Spain <nicholas.spain@stileeducation.com>
2025-09-13 20:40:15 +1000

    Move non-Ruby specific code to a separate crate
    This means I can benchmark the rust specific stuff too and use all the
    tools we've got in that ecosystem (e.g. criterion, etc.).

---
+++ crates/object_id/src/lib.rs
@@ -0,0 +1,133 @@
+use std::{
+    sync::{
+        LazyLock,
+        atomic::{AtomicU32, Ordering},
+    },
+    time::{Duration, SystemTime, UNIX_EPOCH},
+};
+
+static COUNTER: LazyLock<AtomicU32> = LazyLock::new(|| AtomicU32::new(rand::random()));
+
+static MACHINE_ID: LazyLock<[u8; 5]> = LazyLock::new(|| {
+    let mut machine_id = [0; 5];
+    rand::fill(&mut machine_id);
+    machine_id
+});
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("invalid hex string: {0}")]
+    InvalidHexString(#[from] hex::FromHexError),
+}
+
+#[derive(Debug)]
+pub struct ObjectId {
+    timestamp: [u8; 4],
+    machine_id: [u8; 5],
+    counter: [u8; 3],
+}
+
+impl ObjectId {
+    pub fn new() -> Self {
+        Self::from_time(SystemTime::now(), true)
+    }
+
+    pub fn from_time(t: SystemTime, unique: bool) -> Self {
+        let counter = if unique {
+            let counter = COUNTER.fetch_add(1, Ordering::SeqCst);
+            let bs = counter.to_be_bytes();
+            [bs[1], bs[2], bs[3]]
+        } else {
+            [0, 0, 0]
+        };
+        let timestamp = (t
+            .duration_since(UNIX_EPOCH)
+            .expect("time is after unix epoch; that is impossible")
+            .as_secs() as u32)
+            .to_be_bytes();
+        let machine_id = *MACHINE_ID;
+
+        Self {
+            timestamp,
+            machine_id,
+            counter,
+        }
+    }
+
+    pub fn to_bytes(&self) -> [u8; 12] {
+        let mut bs = [0u8; 12];
+        bs[0..4].copy_from_slice(&self.timestamp);
+        bs[4..9].copy_from_slice(&self.machine_id);
+        bs[9..12].copy_from_slice(&self.counter);
+        bs
+    }
+
+    pub fn timestamp(&self) -> SystemTime {
+        SystemTime::UNIX_EPOCH + Duration::from_secs(u32::from_be_bytes(self.timestamp) as u64)
+    }
+
+    pub fn machine_id(&self) -> u64 {
+        let mut padded = [0u8; 8];
+        padded[3..8].copy_from_slice(&self.machine_id);
+        u64::from_be_bytes(padded)
+    }
+
+    pub fn counter(&self) -> u32 {
+        u32::from_be_bytes([0, self.counter[0], self.counter[1], self.counter[2]])
+    }
+}
+
+impl ToString for ObjectId {
+    fn to_string(&self) -> String {
+        let mut bs: [u8; 12] = [0; 12];
+        bs[0..4].copy_from_slice(&self.timestamp);
+        bs[4..9].copy_from_slice(&self.machine_id);
+        bs[9..12].copy_from_slice(&self.counter);
+        hex::encode(bs)
+    }
+}
+
+impl TryFrom<String> for ObjectId {
+    type Error = Error;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        let bs = hex::decode(value)?;
+        // TODO: Validate length
+        let timestamp = [bs[0], bs[1], bs[2], bs[3]];
+        let machine_id = [bs[4], bs[5], bs[6], bs[7], bs[8]];
+        let counter = [bs[9], bs[10], bs[11]];
+        Ok(Self {
+            timestamp,
+            machine_id,
+            counter,
+        })
+    }
+}
+
+impl From<ObjectId> for u128 {
+    fn from(value: ObjectId) -> Self {
+        let bs = value.to_bytes();
+        let mut padded_bs = [0u8; 16];
+        padded_bs[0..12].copy_from_slice(&bs);
+        u128::from_be_bytes(padded_bs)
+    }
+}
+
+impl PartialEq for ObjectId {
+    fn eq(&self, other: &Self) -> bool {
+        self.to_bytes() == other.to_bytes()
+    }
+}
+
+impl Eq for ObjectId {}
+
+impl PartialOrd for ObjectId {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for ObjectId {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.to_bytes().cmp(&other.to_bytes())
+    }
+}
--- ext/penguin_object_id/src/lib.rs
+++ ext/penguin_object_id/src/lib.rs
@@ -1,80 +1,32 @@
-use std::{
-    sync::{
-        atomic::{AtomicU32, Ordering},
-        LazyLock,
-    },
-    time::{Duration, SystemTime, UNIX_EPOCH},
-};
+use std::time::SystemTime;
 
 use magnus::{function, method, prelude::*, Error, Ruby};
 
-static COUNTER: LazyLock<AtomicU32> = LazyLock::new(|| AtomicU32::new(rand::random()));
-
-static MACHINE_ID: LazyLock<[u8; 5]> = LazyLock::new(|| {
-    let mut machine_id = [0; 5];
-    rand::fill(&mut machine_id);
-    machine_id
-});
-
 #[derive(Debug)]
 #[magnus::wrap(class = "Penguin::ObjectId")]
-struct ObjectId {
-    timestamp: [u8; 4],
-    machine_id: [u8; 5],
-    counter: [u8; 3],
-}
+struct ObjectId(object_id::ObjectId);
 
 impl ObjectId {
-    fn generate() -> Result<Self, Error> {
-        Self::generate_from_time(SystemTime::now(), true)
+    fn generate() -> Self {
+        Self(object_id::ObjectId::new())
     }
 
-    fn generate_from_time(t: SystemTime, unique: bool) -> Result<Self, Error> {
-        let counter = if unique {
-            let counter = COUNTER.fetch_add(1, Ordering::SeqCst);
-            let bs = counter.to_be_bytes();
-            [bs[1], bs[2], bs[3]]
-        } else {
-            [0, 0, 0]
-        };
-        let timestamp = (t
-            .duration_since(UNIX_EPOCH)
-            .expect("time is after unix epoch; that is impossible")
-            .as_secs() as u32)
-            .to_be_bytes();
-        let machine_id = *MACHINE_ID;
-
-        Ok(Self {
-            timestamp,
-            machine_id,
-            counter,
-        })
+    fn generate_from_time(t: SystemTime, unique: bool) -> Self {
+        Self(object_id::ObjectId::from_time(t, unique))
     }
 
     fn from_string(ruby: &Ruby, s: String) -> Result<Self, Error> {
-        let bs =
-            hex::decode(s).map_err(|e| Error::new(ruby.exception_arg_error(), e.to_string()))?;
-        // TODO: Validate length
-        let timestamp = [bs[0], bs[1], bs[2], bs[3]];
-        let machine_id = [bs[4], bs[5], bs[6], bs[7], bs[8]];
-        let counter = [bs[9], bs[10], bs[11]];
-        Ok(Self {
-            timestamp,
-            machine_id,
-            counter,
-        })
+        Ok(Self(object_id::ObjectId::try_from(s).map_err(|e| {
+            Error::new(ruby.exception_arg_error(), e.to_string())
+        })?))
     }
 
     fn to_s(&self) -> String {
-        self.to_string()
+        self.0.to_string()
     }
 
     fn to_bytes(&self) -> [u8; 12] {
-        let mut bs = [0u8; 12];
-        bs[0..4].copy_from_slice(&self.timestamp);
-        bs[4..9].copy_from_slice(&self.machine_id);
-        bs[9..12].copy_from_slice(&self.counter);
-        bs
+        self.0.to_bytes()
     }
 
     fn to_i(&self) -> u128 {
@@ -85,51 +37,23 @@ impl ObjectId {
     }
 
     fn timestamp(&self) -> SystemTime {
-        SystemTime::UNIX_EPOCH + Duration::from_secs(u32::from_be_bytes(self.timestamp) as u64)
+        self.0.timestamp()
     }
 
     fn machine_id(&self) -> u64 {
-        let mut padded = [0u8; 8];
-        padded[3..8].copy_from_slice(&self.machine_id);
-        u64::from_be_bytes(padded)
+        self.0.machine_id()
     }
 
     fn counter(&self) -> u32 {
-        u32::from_be_bytes([0, self.counter[0], self.counter[1], self.counter[2]])
+        self.0.counter()
     }
 
     fn compare(&self, other: &Self) -> i8 {
-        self.to_bytes().cmp(&other.to_bytes()) as i8
+        self.0.cmp(&other.0) as i8
     }
-}
 
-impl ToString for ObjectId {
-    fn to_string(&self) -> String {
-        let mut bs: [u8; 12] = [0; 12];
-        bs[0..4].copy_from_slice(&self.timestamp);
-        bs[4..9].copy_from_slice(&self.machine_id);
-        bs[9..12].copy_from_slice(&self.counter);
-        hex::encode(bs)
-    }
-}
-
-impl PartialEq for ObjectId {
     fn eq(&self, other: &Self) -> bool {
-        self.to_bytes() == other.to_bytes()
-    }
-}
-
-impl Eq for ObjectId {}
-
-impl PartialOrd for ObjectId {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for ObjectId {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.to_bytes().cmp(&other.to_bytes())
+        self.0 == other.0
     }
 }
 
